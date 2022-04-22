d2e6c58a642484d5f60abd09f5d220a5f54cdcac
immmdreza <ir310022@gmail.com>
2022-04-22 18:10:04 +0430

    Added secrets

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
+
 from flask import Flask
 
+
 app = Flask(__name__)
+
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 
 @app.route("/")
 def index():
-    return "Hmmm this is my staging app."
+    secret_key = app.config.get("SECRET_KEY")  # type: ignore
+    return f"The configured secret key is {secret_key}."
